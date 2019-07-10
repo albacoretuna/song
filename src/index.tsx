e55c12e18f331fe7ba9a4b8f67e5fbe95bebc15e
Nick Okapi <nick.okapi@relex.fi>
2019-07-10 16:14:41 +0300

    Add IE11 support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 // nothing much hapenning here, go to App.tsx instead
 
+// some IE polyfills
 import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
