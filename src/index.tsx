27451fab7bf35fd51eb800c289e013beedf9dbe5
Nick Okapi <nick.okapi@relex.fi>
2019-07-10 15:55:35 +0300

    Add polyfill for IE9+

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 // nothing much hapenning here, go to App.tsx instead
 
+import 'react-app-polyfill/ie9';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
