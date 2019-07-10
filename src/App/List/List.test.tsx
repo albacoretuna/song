3b5d14cf505a0cafd9603c151860a18416f1fd15
Nick Okapi <nick.okapi@relex.fi>
2019-07-10 08:20:13 +0300

    Add some integration tests

--- src/App/List/List.test.tsx
+++ src/App/List/List.test.tsx
@@ -3,23 +3,23 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 //ours
-import Card from './Card';
+import List from './List';
 
 // mock data
-const song =  {
+const loadedSongs =  [{
     "id": "5b8e47deb3984c68ed8192e3",
     "title": "Easter Oratorio (in Am)",
     "artist": "Johann Sebastian Bach",
     "images": "https://d3mzlbmn9ukddk.cloudfront.net/songs/image/586a75fa-99df-4ce0-af71-55f6a474c404.jpg",
     "level": 6,
     "search": "johann sebastian bach easter oratorio (in am)"
-};
+}];
 
 
 // Just a basic sanity check
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Card song={song} index={1} isFavorite={true} favorites={[]} setFavorites={()=>{}}/>, div);
+  ReactDOM.render(<List loadedSongs={loadedSongs} favorites={[]} setFavorites={()=>{}} setIsFetching={() => {}} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
--- src/App/lib/LevelIndicator.test.tsx
+++ src/App/lib/LevelIndicator.test.tsx
@@ -1,18 +1,14 @@
-// I spent more time on end to end testing using Cypress instead of unit testing
-// just "npm run cypress:open" to see end to end tests run in a real browser
-
-
 //libs
 import React from 'react';
 import ReactDOM from "react-dom";
 
 //ours
-import App from './App';
+import LevelIndicator from './LevelIndicator';
 
 // Just a basic sanity check
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<LevelIndicator level={10} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
